17d0c80c5caa2e0bc5bc868a1578a6df8b713827
David Margolin <davidmargolin0305@gmail.com>
2019-12-02 14:13:39 -0500

    organization

--- 2/index.js
+++ 2/index.js
@@ -1,16 +1,43 @@
 import fs from "fs";
 
-const text = fs.readFileSync("./2/input.txt", { encoding: "utf-8" });
-const textByLine = text.split("\n");
+// parse input
 
-const getFuel = input => {
-  const fuel = Math.floor(parseInt(input) / 3) - 2;
-  if (fuel <= 0) return 0;
-  else return fuel + getFuel(fuel);
+const text = fs.readFileSync("./2/input.txt", { encoding: "utf-8" });
+const memory = text.split(",").map(str => parseInt(str));
+
+// Part 1
+
+const runProgram = mem => {
+  for (let i = 0; mem[i] !== 99; i += 4) {
+    if (mem[i] === 1) {
+      mem[mem[i + 3]] = mem[mem[i + 1]] + mem[mem[i + 2]];
+    } else if (mem[i] == 2) {
+      mem[mem[i + 3]] = mem[mem[i + 1]] * mem[mem[i + 2]];
+    }
+  }
 };
 
-console.log(
-  textByLine.reduce((prev, curr) => {
-    return prev + getFuel(curr);
-  }, 0)
-);
+const memCopy = [...memory];
+memCopy[1] = 12;
+memCopy[2] = 2;
+runProgram(memCopy);
+
+console.log("Part 1:");
+console.log(memCopy[0]);
+
+// Part 2
+
+const EXPECTED_OUT = 19690720;
+for (let noun = 0; noun < 100; noun++) {
+  for (let verb = 0; verb < 100; verb++) {
+    const memCopy = [...memory];
+    memCopy[1] = noun;
+    memCopy[2] = verb;
+    runProgram(memCopy);
+    if (memCopy[0] == EXPECTED_OUT) {
+      console.log("Part 2:");
+      console.log(100 * noun + verb);
+      break;
+    }
+  }
+}
