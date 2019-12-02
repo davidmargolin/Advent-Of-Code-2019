cf2f21a95ab9398b79536f182090e6c1823f21da
David Margolin <davidmargolin0305@gmail.com>
2019-12-02 13:32:26 -0500

    combine part 1 and 2

--- 1/index.js
+++ 1/index.js
@@ -1,10 +1,30 @@
 import fs from "fs";
 
+// parse input
+
 const text = fs.readFileSync("./1/input.txt", { encoding: "utf-8" });
 const textByLine = text.split("\n");
 
+// Part 1
+
+console.log("Part 1:")
 console.log(
   textByLine.reduce((prev, curr) => {
     return prev + (Math.floor(parseInt(curr) / 3) - 2);
+  }, 0)
+);
+
+// Part 2
+
+const getFuel = input => {
+  const fuel = Math.floor(parseInt(input) / 3) - 2;
+  if (fuel <= 0) return 0;
+  else return fuel + getFuel(fuel);
+};
+
+console.log("Part 2:")
+console.log(
+  textByLine.reduce((prev, curr) => {
+    return prev + getFuel(curr);
   }, 0)
 );
