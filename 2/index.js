078b3403b59bd8ac8deab4f07b682b43205b69cf
David Margolin <davidmargolin0305@gmail.com>
2019-12-03 11:11:12 -0500

    getProgramOut returns first element

--- 2/index.js
+++ 2/index.js
@@ -7,7 +7,7 @@ const memory = text.split(",").map(str => parseInt(str));
 
 // Part 1
 
-const runProgram = mem => {
+const getProgramOut = mem => {
   for (let i = 0; mem[i] !== 99; i += 4) {
     if (mem[i] === 1) {
       mem[mem[i + 3]] = mem[mem[i + 1]] + mem[mem[i + 2]];
@@ -15,26 +15,26 @@ const runProgram = mem => {
       mem[mem[i + 3]] = mem[mem[i + 1]] * mem[mem[i + 2]];
     }
   }
+  return mem.shift();
 };
 
 const memCopy = [...memory];
 memCopy[1] = 12;
 memCopy[2] = 2;
-runProgram(memCopy);
 
 console.log("Part 1:");
-console.log(memCopy[0]);
+console.log(getProgramOut(memCopy));
 
 // Part 2
 
 const EXPECTED_OUT = 19690720;
+
 for (let noun = 0; noun < 100; noun++) {
   for (let verb = 0; verb < 100; verb++) {
     const memCopy = [...memory];
     memCopy[1] = noun;
     memCopy[2] = verb;
-    runProgram(memCopy);
-    if (memCopy[0] == EXPECTED_OUT) {
+    if (getProgramOut(memCopy) === EXPECTED_OUT) {
       console.log("Part 2:");
       console.log(100 * noun + verb);
       break;
