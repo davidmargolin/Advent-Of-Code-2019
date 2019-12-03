c0a9ebf2524201349d83f8d833c91040b8ecdf75
David Margolin <davidmargolin0305@gmail.com>
2019-12-03 04:15:35 -0500

    day 3 super efficient answer using Map

--- 3/index.js
+++ 3/index.js
@@ -9,89 +9,56 @@ const [wire0, wire1] = text.split("\n").map(line => line.split(","));
 
 const generateCoordsFromInstructions = instructions => {
   let wireLastCoord = [0, 0];
-  const wireCoords = [];
-  instructions
+  let distance = 0;
+  return instructions
     .map(instruction => instruction.split(""))
-    .forEach(([direction, ...amount], index) => {
+    .reduce((total, [direction, ...amount]) => {
       const amountInt = parseInt(amount.join(""));
-      if (direction === "R") {
-        for (let i = 0; i < amountInt; i++) {
-          const nextCoord = [wireLastCoord[0] + 1, wireLastCoord[1]];
-          wireCoords.push(nextCoord);
-          wireLastCoord = nextCoord;
+      for (let i = 0; i < amountInt; i++) {
+        distance += 1;
+        let nextCoord;
+        if (direction === "R") {
+          nextCoord = [wireLastCoord[0] + 1, wireLastCoord[1]];
+        } else if (direction === "U") {
+          nextCoord = [wireLastCoord[0], wireLastCoord[1] + 1];
+        } else if (direction === "L") {
+          nextCoord = [wireLastCoord[0] - 1, wireLastCoord[1]];
+        } else if (direction === "D") {
+          nextCoord = [wireLastCoord[0], wireLastCoord[1] - 1];
         }
-      } else if (direction === "U") {
-        for (let i = 0; i < amountInt; i++) {
-          const nextCoord = [wireLastCoord[0], wireLastCoord[1] + 1];
-          wireCoords.push(nextCoord);
-          wireLastCoord = nextCoord;
+        if (!total.has(nextCoord.join(","))) {
+          total.set(nextCoord.join(","), distance);
         }
-      } else if (direction === "L") {
-        for (let i = 0; i < amountInt; i++) {
-          const nextCoord = [wireLastCoord[0] - 1, wireLastCoord[1]];
-          wireCoords.push(nextCoord);
-          wireLastCoord = nextCoord;
-        }
-      } else if (direction === "D") {
-        for (let i = 0; i < amountInt; i++) {
-          const nextCoord = [wireLastCoord[0], wireLastCoord[1] - 1];
-          wireCoords.push(nextCoord);
-          wireLastCoord = nextCoord;
-        }
+        wireLastCoord = nextCoord;
       }
-    });
-  return wireCoords;
+      return total;
+    }, new Map());
 };
 
 const wire0Coords = generateCoordsFromInstructions(wire0);
 const wire1Coords = generateCoordsFromInstructions(wire1);
 
-console.log("Part 1:")
-console.log(
-  Math.min(
-    ...wire0Coords
-      .filter(([x, y]) => wire1Coords.find(([x1, y1]) => x1 === x && y1 === y))
-      .map(([x, y]) => Math.abs(x) + Math.abs(y))
-  )
-);
+let minIntersection = Infinity;
+for (const coord of wire0Coords.keys()) {
+  if (wire1Coords.has(coord)) {
+    const [x, y] = coord.split(",").map(s => parseInt(s));
+    const manhattanDistance = Math.abs(x) + Math.abs(y);
+    if (manhattanDistance < minIntersection)
+      minIntersection = manhattanDistance;
+  }
+}
+console.log("Part 1:");
+console.log(minIntersection);
 
 // Part 2
 
-const wire0Coords = generateCoordsFromInstructions(wire0).map(
-  (item, index) => ({
-    coord: item,
-    index
-  })
-);
-
-const wire1Coords = generateCoordsFromInstructions(wire1).map(
-  (item, index) => ({
-    coord: item,
-    index
-  })
-);
-
-let companion0 = null;
-const shortest0 = wire0Coords.findIndex(({ coord: [x, y] }) => {
-  const index = wire1Coords.findIndex(
-    ({ coord: [x1, y1] }) => x1 === x && y1 === y
-  );
-  if (index !== -1) {
-      companion0 = index;
-      return index
+let minDistance = Infinity;
+for (const [coord, distance] of wire0Coords.entries()) {
+  if (wire1Coords.has(coord)) {
+    const totalDistance = distance + wire1Coords.get(coord);
+    if (totalDistance < minDistance) minDistance = totalDistance;
   }
-});
-
-let companion1 = null;
-const shortest1 = wire1Coords.findIndex(({ coord: [x, y] }) => {
-  const index = wire0Coords.findIndex(
-    ({ coord: [x1, y1] }) => x1 === x && y1 === y
-  );
-  if (index !== -1) {
-    companion1 = index;
-    return index
 }
-});
 
 console.log("Part 2:");
-console.log(Math.min(shortest0 + companion0, shortest1 + companion1) + 2);
+console.log(minDistance);
